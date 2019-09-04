e670207b6e742fbc80045a9681a36969ebc2d579
MaheKarim <mahekarim@gmail.com>
2019-09-04 21:03:25 +0600

    Update

--- resources/views/todos/home.blade.php
+++ resources/views/todos/home.blade.php
@@ -1,68 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{--  <link rel="stylesheet" type="text/css" href="css/milligram.css">  --}}
+@extends('layouts.app')
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <title>ToDo Application</title>
-</head>
-<body>
-
-
-    <div class="container">
-            <!-- Just a navbar start -->
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">ToDo Application</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                  <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                  </ul>
-                  <span class="navbar-text">
-                    Navbar text with an inline element
-                  </span>
-                </div>
-              </nav>
-
-            <!-- Just a navbar end -->
-
-            <div class="card-header">
-                    <p class="text-center" style="font-size: 28px">
-                       ToDo Application
-                    </p>
-
-                </div>
-
-
-
-                <div class="row justify-content-center my-3">
-
-
-                    <div class="card-body">
-                             <ul class="list-group">
-                                @foreach ($todos as $todo )
-                                  <li class="list-group-item">
-                                    {{ $todo->name }}
-                                     <button type="button" class="btn btn-primary btn-sm float-right">View Data</button>
-                                  </li>
-                                  @endforeach
-                             </ul>
-                    </div>
-
-       </div>
+@section('content')
+<!-- Just a navbar start -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">ToDo Application</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+      <span class="navbar-text">
+        Navbar text with an inline element
+      </span>
     </div>
-</body>
-</html>
+  </nav>
+
+<!-- Just a navbar end -->
+
+<div class="card-header">
+        <p class="text-center" style="font-size: 28px">
+           ToDo Application
+        </p>
+
+    </div>
+
+
+
+    <div class="row justify-content-center my-3">
+
+
+        <div class="card-body">
+                 <ul class="list-group">
+                    @foreach ($todos as $todo )
+                      <li class="list-group-item">
+                        {{ $todo->name }}
+                         <a href="/todos/{{ $todo->id }}" type="button" class="btn btn-primary btn-sm float-right">View Data</a >
+                      </li>
+                      @endforeach
+                 </ul>
+        </div>
+
+</div>
+@endsection
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -8,6 +8,25 @@
     <title>ToDo | Show</title>
 </head>
 <body>
-     {{ $todo->name }}
+
+
+     <div class="container">
+         <h1 class="text-center my-5">
+            {{ $todo->name }}
+         </h1>
+
+     <div class="row justify-content-center">
+         <div class="col-md-6">
+             <div class="card card-default">
+                 <div class="card-header">
+                    Details
+                 </div>
+                 <div class="card-body">
+                     {{ $todo->description }}
+                 </div>
+             </div>
+         </div>
+     </div>
+    </div>
 </body>
 </html>
