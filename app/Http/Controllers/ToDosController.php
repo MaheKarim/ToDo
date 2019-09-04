48f9b985229b1255cf41d94083a7008bfc9097fa
MaheKarim <mahekarim@gmail.com>
2019-09-04 17:39:22 +0600

    Design Done

--- app/Http/Controllers/ToDosController.php
+++ app/Http/Controllers/ToDosController.php
@@ -16,3 +16,5 @@ class ToDosController extends Controller
 
    }
 }
+
+
--- resources/views/todos/home.blade.php
+++ resources/views/todos/home.blade.php
@@ -11,10 +11,51 @@
 </head>
 <body>
 
-    <div class="card card-default">
-        <div class="card-header">
-          Todo Application
-        </div>
+
+    <div class="container">
+            <!-- Just a navbar start -->
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">ToDo Application</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                  </ul>
+                  <span class="navbar-text">
+                    Navbar text with an inline element
+                  </span>
+                </div>
+              </nav>
+
+            <!-- Just a navbar end -->
+            <div class="row justify-content-center my-5">
+            <div class="card card-default">
+                    <div class="card-header">
+                        <p class="text-center" style="font-size: 28px">
+                           ToDo Application
+                        </p>
+
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            @foreach ($todos as $todo )
+                              <li class="list-group-item">{{ $todo->name }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+
+       </div>
     </div>
 
 
